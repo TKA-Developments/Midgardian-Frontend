85dc55a4a1372c46fbe7638d0a23319534f56708
Andra Antariksa <andra.antariksa@gmail.com>
2022-04-20 14:40:19 +0700

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,18 +27,42 @@ function MyApp({ Component, pageProps }: AppProps) {
           `,
           }}
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/img/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/img/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
-      <div className="flex justify-center items-center bg-background">
-        <div className="max-w-[1920px] w-full">
+      <main className="flex justify-center items-center bg-background overflow-hidden">
+        <div className=" max-w-[1920px] w-full">
           <Navbar />
-          <Component {...pageProps} />
-          <Footer />
+          <div className="pt-[76px]">
+            <Component {...pageProps} />
+            <Footer />
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
