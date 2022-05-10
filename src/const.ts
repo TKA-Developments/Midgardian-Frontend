a66d6c634554c72b72993be135567d6b8fa46f9f
Andra Antariksa <andra.antariksa@gmail.com>
2022-05-10 15:10:13 +0700

    Change environment variable to hardcode

--- src/const.ts
+++ src/const.ts
@@ -6,5 +6,5 @@ export const TWITTER_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 
 export const SMARTCONTRACT_ADDRESS =
   "0x73920fe556a604da68165634a334e9760760a80e";
-// ${process.env.INFURA_PROJECT_ID}
-export const ETHEREUM_RPC_ENDPOINT = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+export const INFURA_PROJECT_ID = "03498e4fc66049e5b8a7454d29522aad";
+export const ETHEREUM_RPC_ENDPOINT = `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`;
